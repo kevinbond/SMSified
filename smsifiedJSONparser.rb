beb0aaaa415549dc8f844dc0b2ad845804076fb6
Kevin Bond <kbond@voxeo.com>
2011-05-24 13:14:22 -0700

    Edited smsifiedJSONparser.rb via GitHub

--- smsifiedJSONparser.rb
+++ smsifiedJSONparser.rb
@@ -6,12 +6,9 @@ require 'smsified'
 post '/' do  
 
   response = request.env["rack.input"].read
-  puts response
   response = JSON.parse(response)
   
-  puts response
-  
-  myPhoneNumber = 19548258327
+  myPhoneNumber = 1xxxxxxxxxx
   
   message =  response["inboundSMSMessageNotification"]["inboundSMSMessage"]["message"]
   callerID =  response["inboundSMSMessageNotification"]["inboundSMSMessage"]["senderAddress"]
@@ -25,9 +22,9 @@ post '/' do
     message = callerID + " " + message
 
   end
-  url = URI.parse('http://api.smsified.com/v1/smsmessaging/outbound/15853260832/requests')
+  url = URI.parse('http://api.smsified.com/v1/smsmessaging/outbound/{SMSified_phone_number}/requests')
   req = Net::HTTP::Post.new(url.path)
-  req.basic_auth 'kbond', 'kb032811'
+  req.basic_auth 'username', 'password'
   req.set_form_data({'address' => "#{to}", 'message' => "#{message}"})
   res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
 
